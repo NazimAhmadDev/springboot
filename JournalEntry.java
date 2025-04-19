5f9ed284b29060356151278b79453a0605b64365
NazimAhmadDev <nazimahmedchd@gmail.com>
2025-04-19 11:49:26 +0530

    [Nazim] add : cosntructor created

---
+++ JournalEntry.java
@@ -0,0 +1,39 @@
+package com.springDemo.Journal.App;
+
+public class JournalEntry {
+    private long id;
+
+    private String title;
+
+    private String content;
+
+    public void setId(long id) {
+
+        this.id = id;
+    }
+    public long getId() {
+
+        return id;
+    }
+
+
+    public String getTitle() {
+
+        return title;
+    }
+    public void setTitle(String title) {
+
+        this.title = title;
+    }
+
+
+    public void setContent(String content) {
+
+        this.content = content;
+    }
+    public String getContent() {
+
+        return content;
+    }
+
+}
